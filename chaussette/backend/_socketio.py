d85b6d8916f7f82dc3f8c0abfef723614919fd4b
Tarek Ziade <tarek@ziade.org>
2014-04-29 10:11:20 +0200

    added a socketio example and simplified the implementation

--- chaussette/backend/_socketio.py
+++ chaussette/backend/_socketio.py
@@ -4,14 +4,14 @@ from chaussette.util import create_socket
 
 from socketio.server import SocketIOServer
 from socketio.handler import SocketIOHandler
-from gevent.pywsgi import WSGIServer
+from socketio.policyserver import FlashPolicyServer
 
 
 class _SocketIOHandler(SocketIOHandler):
-    def __init__(self, sock, address, server, rfile=None):
+    def __init__(self, config, sock, address, server, rfile=None):
         if server.socket_type == socket.AF_UNIX:
             address = ['0.0.0.0']
-        SocketIOHandler.__init__(self, sock, address, server, rfile)
+        SocketIOHandler.__init__(self, config, sock, address, server, rfile)
 
 
 class Server(SocketIOServer):
@@ -20,24 +20,31 @@ class Server(SocketIOServer):
     socket_type = socket.SOCK_STREAM
     handler_class = _SocketIOHandler
 
-    def __init__(self, listener, application=None, backlog=2048,
-                 socket_type=socket.SOCK_STREAM, spawn='default',
-                 handler_class=None, environ=None,
-                 log='default', address_family=socket.AF_INET, **config):
-        self.address_family = address_family
-        self.socket_type = socket_type
-        host, port = listener
-        self.socket = create_socket(host, port, self.address_family,
-                                    self.socket_type, backlog=backlog)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.application = application
-        self.sockets = {}
-        if 'namespace' in config:
-            self.resource = config.pop('namespace', 'socket.io')
-        else:
-            self.resource = config.pop('resource', 'socket.io')
+    def __init__(self, *args, **kwargs):
+        address_family = kwargs.pop('address_family', socket.AF_INET)
+        socket_type = kwargs.pop('socket_type', socket.SOCK_STREAM)
+        backlog = kwargs.pop('backlog', 2048)
 
-        self.transports = config.pop('transports', None)
-        self.policy_server = None
-        WSGIServer.__init__(self, self.socket, application, None, spawn,
-                            log, self.handler_class, environ, **config)
+        listener = args[0]
+        if isinstance(listener, tuple):
+            host, port = listener
+            _socket = create_socket(host, port, address_family,
+                                    socket_type, backlog=backlog)
+            _socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            args = [_socket] + list(args[1:])
+        else:
+            # it's already a socket..
+            host, port = listener.getsockname()
+
+        # socketio makes the assumption that listener is a host/port
+        # tuple, which is a false assumption, it can be a socket.
+        # it uses listener in its constructor to set the policy server
+        #
+        # Let's set it ourselves here afterwards.
+        old_policy_server = kwargs.pop('policy_server', True)
+        kwargs['policy_server'] = False
+        super(Server, self).__init__(*args, **kwargs)
+
+        if old_policy_server:
+            policylistener = kwargs.pop('policy_listener', (host, 10843))
+            self.policy_server = FlashPolicyServer(policylistener)
---
+++ examples/simple_chat/README.txt
@@ -0,0 +1,1 @@
+Example taken and adapted from https://github.com/abourget/gevent-socketio/tree/master/examples/simple_chat
